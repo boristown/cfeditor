1b97c455d9d68cb113918e4548d288812b2c0cb5
Boris Town <boristown@gmail.com>
2022-08-28 16:44:57 +0800

    实现智能输入解析

---
+++ cfinput.py
@@ -0,0 +1,56 @@
+
+def cfinput(formats):
+    '''
+    cf input data smart loader
+    formart definition.
+    Number of samples: T
+    Single variable：ABCDEFGHIJK
+    Horizontal list: L
+    numeric matrix: NX (X indicates the number of rows)
+    Vertical list: VX (X indicates the number of rows)
+    String: S
+    String matrix: MX (X indicates the number of rows)
+    Separator: Comma(,)
+    '''
+    def get_lines(vn):
+        if vn in vmap:
+            return vmap[vn]
+        else:
+            return int(vn)
+    if formats[0] == 'T':
+        test = int(input())
+        for _ in test:
+            yield cfinput(formats[2:])
+    else:
+        ans = []
+        vmap = {}
+        fs = formats.split(',')
+        for line in fs:
+            if line[0] in 'ABCDEFGHIJK': #Single variable：ABCDEFGHIJK
+                vars = list(map(int, input().split()))
+                for i,v in enumerate(vars):
+                    vmap[line[0][i]]=v
+                ans.extend(vars)
+            elif line[0] == 'L': #Horizontal list: L
+                L = list(map(int, input().split()))
+                ans.append(L)
+            elif line[0] == 'S': #String: S
+                S = input()
+                ans.append(S)
+            elif line[0] == 'N': #numeric matrix: NX (X indicates the number of rows)
+                N = []
+                for _ in range(get_lines(line[1])):
+                    L = list(map(int, input().split()))
+                    N.append(L)
+                ans.append(N)
+            elif line[0] == 'M': #String matrix: MX (X indicates the number of rows)
+                M = []
+                for _ in range(get_lines(line[1])):
+                    M.append(input())
+                ans.append(M)
+            elif line[0] == 'V': #Vertical list: VX (X indicates the number of rows)
+                V = []
+                for _ in range(get_lines(line[1])):
+                    V.append(int(input()))
+                ans.append(V)
+        return ans
---
+++ sample1.py
@@ -0,0 +1,9 @@
+from cfinput import *
+
+class Solution:
+    def solve(a,b,c,d,M,V):
+        pass
+
+if __name__ == '__main__':
+    tp = cfinput('ABCD,MC,VD')
+    Solution.solve(*tp)
