a8e01520f7d886b3c523d7ed40de37be50a3b8c0
AK自动机 <47687653+boristown@users.noreply.github.com>
2023-01-27 18:10:18 +0800

    Add files via upload

--- ATC_A.c
+++ ATC_A.c
@@ -44,17 +44,13 @@ void solve(){
 }
 
 int main(){
-    int t;
-    scanf("%d", &t);
-    for(int i = 1; i <= t; i++){
-        #ifdef _local_debug_
-            clock_t start = clock();
-        #endif
-        solve();
-        #ifdef _local_debug_
-            clock_t end = clock();
-            printf("Case #%d: %lf ms\n", i, (double)(end - start) / CLOCKS_PER_SEC * 1000);
-        #endif
-    }
+    #ifdef _local_debug_
+        clock_t start = clock();
+    #endif
+    solve();
+    #ifdef _local_debug_
+        clock_t end = clock();
+        printf("Time: %lf ms\n", (double)(end - start) / CLOCKS_PER_SEC * 1000);
+    #endif
     return 0;
 }
--- ATC_A.cpp
+++ ATC_A.cpp
@@ -50,17 +50,13 @@ void solve(){
 }
 
 int main(){
-    int t;
-    cin >> t;
-    for(int i = 1; i <= t; i++){
-        #ifdef _local_debug_
-            clock_t start = clock();
-        #endif
-        solve();
-        #ifdef _local_debug_
-            clock_t end = clock();
-            cout << "Case #" << i << ": " << (double)(end - start) / CLOCKS_PER_SEC * 1000 << " ms" << endl;
-        #endif
-    }
+    #ifdef _local_debug_
+        clock_t start = clock();
+    #endif
+    solve();
+    #ifdef _local_debug_
+        clock_t end = clock();
+        cout << "Time:" << (double)(end - start) / CLOCKS_PER_SEC * 1000 << " ms" << endl;
+    #endif
     return 0;
 }
